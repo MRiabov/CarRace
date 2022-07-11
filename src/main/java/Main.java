9fbdab05814b45a9e39eeb7458f9e8f691b4ac7e
MRiabov <maksymriabov2004@gmail.com>
2022-07-11 19:41:04 +0100

    It works perfectly fine. I'm happy.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Main {
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
+public class Main {
+    public static CyclicBarrier startLine = new CyclicBarrier(11);
+    public static CyclicBarrier finishLine = new CyclicBarrier(10);
+    public static Semaphore tunnel = new Semaphore(3);
+    public static int winner;
+    public static final Object WINNER_MONITOR=new Object();
+
+    public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new Car());
+            thread.start();
+        }
+        rideBegin();
+        long now=System.currentTimeMillis();
+        rideEnd();
+    }
+
+    private static void rideBegin() throws BrokenBarrierException, InterruptedException {
+        startLine.await();
+        System.out.println("BEGIN!!!");
+    }
+    private static void rideEnd() throws BrokenBarrierException, InterruptedException {
+        finishLine.await();
+        System.out.println("The end");
+    }
 }
